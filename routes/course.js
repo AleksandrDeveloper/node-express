153b017d2bbda6e316ff84a734bfc176caf392be
AleksandrDeveloper <aleks777.chernof7@mail.ru>
2020-09-05 18:37:47 +0300

    show course

--- routes/course.js
+++ routes/course.js
@@ -5,6 +5,7 @@ const router = Router()
 
 router.get('/',async (req,res)=>{
     const courses =  await Course.getAll()
+    console.log(courses);
     res.render('courses',{
         title:'Courses',
         subTitle:'Courses page',
